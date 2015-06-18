1ddb7170ce714407e9d4edb01f7d638f38736f3d
Blake Sheridan <blake.m.sheridan@gmail.com>
2015-06-17 22:24:35 -0400

    strcmp: out of date

--- strcmp.h
+++ strcmp.h
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <feature/_HAS_BUILTIN_STRCMP.h>
+#include <feature/__has_builtin_strcmp.h>
 
 static inline
 int
 strcmp(const char* s1, const char* s2)
 {
-#if _HAS_BUILTIN_STRCMP
+#if __has_builtin_strcmp
     return __builtin_strcmp(s1, s2);
 #else
 #  error
